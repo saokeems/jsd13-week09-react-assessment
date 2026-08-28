93357722346351a262a15c9b0a85f1921d95db47
saokeems <saokeems@gmail.com>
2026-08-28 11:48:37 +0700

    add section toggle and basic table layout

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,11 +1,15 @@
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
 
 const NavBar = () => {
   return (
     <div className="flex justify-end gap-4 font-bold py-4 px-8 border-b text-lg">
-      <Link to="/">Home</Link>
-      <Link to="/owner">Owner</Link>
+      <Link to="/" className="hover:text-blue-500">
+        Home
+      </Link>
+      <Link to="/owner" className="hover:text-blue-500">
+        Owner
+      </Link>
     </div>
-  )
-}
-export default NavBar
+  );
+};
+export default NavBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,24 +1,58 @@
 import { useState } from "react";
 
 const Home = () => {
-
-  const [section, setSection] = useState("")
+  const [section, setSection] = useState("normal");
 
   return (
-    <div>
+    <div className="w-full px-4">
       <h1 className="text-4xl font-bold text-center mb-8">
         <span className="block">Generation Thailand</span>
-        <span className="block">React - Assessment</span>
+        {section === "normal" && (
+          <span className="block">React - Assessment</span>
+        )}
+        {section === "user" && (
+          <span className="block">Home - User Section</span>
+        )}
+        {section === "admin" && (
+          <span className="block">Home - Admin Section</span>
+        )}
       </h1>
 
-      <div className="flex gap-12 mb-12">
-        <button className="bg-white px-5 py-3 rounded-md font-bold text-gray-800 shadow-md hover:bg-gray-100">
+      <div className="flex gap-12 mb-12 justify-center">
+        <button
+          onClick={() => setSection("user")}
+          className="bg-white px-5 py-3 rounded-md font-bold text-gray-800 shadow-md hover:bg-gray-100"
+        >
           User Home Section
         </button>
-        <button className="bg-white px-5 py-3 rounded-md font-bold text-gray-800 shadow-md hover:bg-gray-100">
+        <button
+          onClick={() => setSection("admin")}
+          className="bg-white px-5 py-3 rounded-md font-bold text-gray-800 shadow-md hover:bg-gray-100"
+        >
           Admin Home Section
         </button>
       </div>
+
+      {section !== "normal" && (
+        <div className="w-full max-w-2xl mx-auto">
+          <table className="w-full border bg-white shadow-sm table-fixed">
+            <thead>
+              <tr className="border-b">
+                <th className="border-r text-center bg-gray-200">Name</th>
+                <th className="border-r text-center bg-gray-200">Last Name</th>
+                <th className="border-r text-center bg-gray-200">Position</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr className="border-b">
+                <td className="border-r text-center bg-white">Test</td>
+                <td className="border-r text-center bg-white"></td>
+                <td className="border-r text-center bg-white"></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      )}
     </div>
   );
 };
